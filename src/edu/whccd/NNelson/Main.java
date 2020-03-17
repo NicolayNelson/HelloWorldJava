113a391fcfcb24ccabf7093bd83730321b01c4a2
MyCraftablRomnce@gmail.com <Booger12!>
2020-03-17 09:21:18 -0700

    Added the "Hello World" code.

---
+++ src/edu/whccd/NNelson/Main.java
@@ -0,0 +1,11 @@
+package edu.whccd.NNelson;
+/*
+    The purpose of this application is to display Hello World!
+    author: Nicolay Nelson
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
